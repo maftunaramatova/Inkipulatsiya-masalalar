6382e026a9eb8705043728b08597e6aae64d59df
senyoramoon010 <djumaniyazovumarjon@gmail.com>
2026-02-18 18:04:57 +0500

    Add files via upload

---
+++ 18.02.2026.py
@@ -0,0 +1,99 @@
+##1-masala:
+# class BankHisobi:
+#     def __init__(self,egasi,boshlangich_balans=0):
+#         self.__egasi = egasi
+#         self.__balans = boshlangich_balans
+#
+#     def get_egasi(self):
+#         return self.__egasi
+#
+#     def get_boshlangich_balans(self):
+#         return self.__balans
+#
+#     def pul_qosh(self,miqdor):
+#         if miqdor <= 0:
+#             print("Noto'g'ri miqdor")
+#             return
+#         self.__balans += miqdor
+#
+#     def pul_yech(self,miqdor):
+#         if miqdor <= 0:
+#             print("Noto'g'ri miqdor")
+#         elif miqdor > self.__balans:
+#             print("Mablag' yetarli emas!")
+#         else:
+#             self.__balans -= miqdor
+#
+# hisobi = BankHisobi("Maftuna",5_000_000)
+# print(hisobi.get_egasi())
+# print(hisobi.get_boshlangich_balans())
+# hisobi.pul_qosh(200_000)
+# print(hisobi.get_boshlangich_balans())
+# hisobi.pul_yech(200_000)
+# print(hisobi.get_boshlangich_balans())
+# hisobi.pul_yech(200_000)
+# hisobi.pul_qosh(-100_000)
+
+##2-masala:
+# class Foydalanuvchi:
+#     def __init__(self,ism,parol):
+#         self.ism=ism
+#         self.__parol=parol
+#
+#     def get_ism(self):
+#         return self.ism
+#
+#     def get_parol(self):
+#         return self.__parol
+#
+#     def parolni_tekshir(self,kiritilgan_parol):
+#         if self.__parol == kiritilgan_parol:
+#             return True
+#         else:
+#             return False
+#
+#     def parolni_ozgartir(self,eski_parol,yangi_parol):
+#         if self.__parol != eski_parol:
+#             print("Eski parol noto'g'ri")
+#             return False
+#         elif len(yangi_parol) < 6:
+#             print("yangi parol 6 ta belgidan iborat bo'lishi kerak")
+#             return False
+#         else:
+#             print("Parol muvofiqaytli o'zgartirildi")
+#             return True
+# user = Foydalanuvchi("Maftuna","maxfiy123")
+# print(user.get_parol())
+# print(user.parolni_tekshir("noto'g'ri"))
+# print(user.parolni_tekshir("maxfiy123"))
+#
+# user.parolni_ozgartir("xato","yangi456")
+# user.parolni_ozgartir("maxfiy123","123")
+# user.parolni_ozgartir("maxfiy123","yangiParoli")
+#
+# print(user.parolni_tekshir("maxfiy123"))
+# print(user.parolni_tekshir("yangiParoli"))
+
+##3-masala:
+# class Talaba:
+#     def __init__(self,ism,fan):
+#         self.__ism = ism
+#         self.__fan = fan
+#         self.__baholar = ()
+#
+#     def
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
