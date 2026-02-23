754e717994a63e098c1560317b4a0ae74affe604
senyoramoon010 <djumaniyazovumarjon@gmail.com>
2026-02-23 18:10:10 +0500

    Add files via upload

--- masalalar-yechimi.py
+++ masalalar-yechimi.py
@@ -118,7 +118,63 @@
 #
 
 ##4-Masala:
-
+# class HaroratSensori:
+#     def __init__(self,sensor_id,joylashuv):
+#         self.__sensor_id = sensor_id
+#         self.__joylashuv = joylashuv
+#         self.__olchovlar = []
+#         self.__min_harorat = -50
+#         self.__max_harorat = 150
+#
+#     def get_sensor_id(self):
+#         return self.__sensor_id
+# 
+#     def get_joylashuv(self):
+#         return self.__joylashuv
+#
+#     def olchov_qosh(self,harorat,vaqt):
+#         if not(self.__min_harorat <= harorat <= self.__max_harorat):
+#             print("Xato: harorat -50 dan 150 gacha bo'lishi kerak!")
+#         else:
+#             self.__olchovlar.append({"harorat":harorat,"vaqt":vaqt})
+#
+#     def oxirgi_olchov(self):
+#         if not self.__olchovlar:
+#             return "Hali o'lchov yo'q"
+#         return self.__olchovlar[-1].copy()
+#
+#     def ortacha_harorat(self):
+#         if not self.__olchovlar:
+#             return 0.0
+#         jami = sum(o["harorat"] for o in self.__olchovlar)
+#         return round( jami / len(self.__olchovlar),2)
+#
+#     def olchovlar_soni(self):
+#         return len(self.__olchovlar)
+#
+#     def hisobot(self):
+#         oxirgi = self.oxirgi_olchov()
+#         if isinstance(oxirgi, str):
+#             oxirgi_str = "yo'q"
+#         else:
+#             oxirgi_str = f"{oxirgi['harorat']} C,({oxirgi['vaqt']})"
+#         return (
+#             f"Sensor:{self.__sensor_id}|Joylashuv:{self.__joylashuv}"
+#             f"O'lchovlar soni:{self.__olchovlar}\n"
+#             f"O'rtacha harorat:{self.ortacha_harorat()}C\n"
+#             f"Oxirgi o'lchov:{oxirgi_str}\n"
+#         )
+#
+# sensor = HaroratSensori("s-001","1-1")
+# sensor.olchov_qosh(22.5,"08:00")
+# sensor.olchov_qosh(25.3,"12:00")
+# sensor.olchov_qosh(19.8,"18:00")
+# sensor.olchov_qosh(200,"20:00")
+#
+# print(sensor.oxirgi_olchov())
+# print(sensor.ortacha_harorat())
+# print(sensor.olchovlar_soni())
+# print(sensor.hisobot())
 
 
 
